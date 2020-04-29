e10e23b132a64ae402a4fbc4d683642471747fcb
Matthew Huang <jiezeng.huang@hotmail.com>
2020-04-29 11:12:52 +0100

    fix bug

--- main.cpp
+++ main.cpp
@@ -9,14 +9,17 @@ using namespace std;
 using namespace std::chrono_literals;
 
 int main(int argc, char** argv)
-{    
-    do
-    {
-        cout << "Distance: " << SmartCar::Instance().get_distance() << endl;
-        cout << "Left: " << SmartCar::Instance().get_infrared_sensor(SENSOR_LEFT_INFRARED) << endl;
-        cout << "Right:" << SmartCar::Instance().get_infrared_sensor(SENSOR_RIGHT_INFRARED) << endl;
-        std::this_thread::sleep_for(100ms);
-    } while (true);
+{   
+    if(SmartCar::Instance().init())
+    { 
+    	do
+    	{
+        	cout << "Distance: " << SmartCar::Instance().get_distance() << endl;
+        	cout << "Left: " << SmartCar::Instance().get_infrared_sensor(SENSOR_LEFT_INFRARED) << endl;
+        	cout << "Right:" << SmartCar::Instance().get_infrared_sensor(SENSOR_RIGHT_INFRARED) << endl;
+        	std::this_thread::sleep_for(100ms);
+    	} while (true);
+    }
     
     return 0;
 }
