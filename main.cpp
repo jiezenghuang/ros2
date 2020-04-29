1d73e6a9a9e6f42a08a08ba8380941163394b58e
Matthew Huang <jiezeng.huang@hotmail.com>
2020-04-29 17:55:40 +0800

    add local project

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include "raspberrypi_car/include/car_device.h"
+#include "raspberrypi_car/include/smart_car.h"
+
+using namespace std;
+using namespace std::chrono_literals;
+
+int main(int argc, char** argv)
+{    
+    do
+    {
+        cout << "Distance: " << SmartCar::Instance().get_distance() << endl;
+        cout << "Left: " << SmartCar::Instance().get_infrared_sensor(SENSOR_LEFT_INFRARED) << endl;
+        cout << "Right:" << SmartCar::Instance().get_infrared_sensor(SENSOR_RIGHT_INFRARED) << endl;
+        std::this_thread::sleep_for(100ms);
+    } while (true);
+    
+    return 0;
+}
